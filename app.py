47a1db5f498f7b9e009042b45334e8d4effa41f3
Zzcool8 <45108550+Zzcool8@users.noreply.github.com>
2021-02-23 14:21:40 -0500

    fixed indent error in upload to aws

--- app.py
+++ app.py
@@ -51,10 +51,8 @@ def get_label():
     })
 
   
-  def upload_to_aws(local_file, bucket, s3_file):
-    s3 = boto3.client('s3', aws_access_key_id= ACCESS_KEY,
-                      aws_secret_access_key= SECRET_KEY)
-
+def upload_to_aws(local_file, bucket, s3_file):
+    s3 = boto3.client('s3', aws_access_key_id= ACCESS_KEY, aws_secret_access_key= SECRET_KEY)
     try:
         s3.upload_file(local_file, bucket, s3_file)
         print("Upload Successful")
