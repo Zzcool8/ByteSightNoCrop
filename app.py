f0c3bb130119f98b4a60b3b29b6cfd685b22865d
Zzcool8 <45108550+Zzcool8@users.noreply.github.com>
2021-02-23 17:13:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import numpy as np
 import tensorflow as tf
 import onnxruntime
 import cv2
-import boto3
-from botocore.exceptions import NoCredentialsError
+#import boto3
+#from botocore.exceptions import NoCredentialsError
 ##import logging
 ##import boto3
 ##from botocore.exceptions import ClientError
@@ -53,18 +53,18 @@ def get_label():
     })
 
   
-def upload_to_aws(local_file, bucket, s3_file):
-    s3 = boto3.client('s3', aws_access_key_id= ACCESS_KEY, aws_secret_access_key= SECRET_KEY)
-    try:
-        s3.upload_file(local_file, bucket, s3_file)
-        print("Upload Successful")
-        return True
-    except FileNotFoundError:
-        print("The file was not found")
-        return False
-    except NoCredentialsError:
-        print("Credentials not available")
-        return False
+#def upload_to_aws(local_file, bucket, s3_file):
+#    s3 = boto3.client('s3', aws_access_key_id= ACCESS_KEY, aws_secret_access_key= SECRET_KEY)
+#    try:
+#        s3.upload_file(local_file, bucket, s3_file)
+#        print("Upload Successful")
+#        return True
+#    except FileNotFoundError:
+#        print("The file was not found")
+#        return False
+#    except NoCredentialsError:
+#        print("Credentials not available")
+#        return False
 
 
 #def upload_file(file_name, bucket, object_name=None):
